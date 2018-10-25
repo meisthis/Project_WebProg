06d4b0daafa3865ad9504d4b72c026a1a0232b73
meisthis <43679703+meisthis@users.noreply.github.com>
2018-10-25 21:34:33 +0700

    update 2

---
+++ main.js
@@ -0,0 +1,156 @@
+var meatArray = ["Beef", "Pork", "Chicken", "Lamb"];
+var vegetableArray = ["Fumak", "Chaisim", "Khailan"];
+var spiceArray = ["Pepper", "Salt", "Sugar"];
+var noodleArray = ["Udon", "Ramen"];
+var fruitArray = ["Apple", "Pineapple", "Watermelon", "Pear"];
+var etcArray = ["Soda", "Beer"]
+
+myDiv = document.getElementById("input-checkbox");
+var flag =0;
+
+function clearDiv(myDiv){
+	document.getElementById(myDiv).innerHTML = "";
+}
+
+function labelMeat(){
+	if(flag != 1){
+		clearDiv("input-checkbox");
+		flag=1;
+
+		for(var i =0; i < meatArray.length; i++){
+			var checkBox = document.createElement("input");
+			var label = document.createElement("label");
+			var myEnter2 = document.createElement("br");
+			var myEnter1 = document.createElement("br");
+
+			checkBox.type = "checkbox";
+			checkBox.value = meatArray[i];
+
+			myDiv.appendChild(myEnter1);
+			myDiv.appendChild(checkBox);
+			myDiv.appendChild(label);
+			myDiv.appendChild(myEnter2);
+			label.appendChild(document.createTextNode(meatArray[i]));
+		}
+	}
+}
+
+function labelVegetable(){	
+	if(flag != 2){
+		clearDiv("input-checkbox");
+		flag=2;
+
+		for(var i =0; i < vegetableArray.length; i++){
+			var checkBox = document.createElement("input");
+			var label = document.createElement("label");
+			var myEnter2 = document.createElement("br");
+			var myEnter1 = document.createElement("br");
+
+			checkBox.type = "checkbox";
+			checkBox.value = vegetableArray[i];
+
+			myDiv.appendChild(myEnter1);
+			myDiv.appendChild(checkBox);
+			myDiv.appendChild(label);
+			myDiv.appendChild(myEnter2);
+			label.appendChild(document.createTextNode(vegetableArray[i]));
+		}
+	}
+}
+
+function labelSpice(){	
+	if(flag != 3){
+		clearDiv("input-checkbox");
+		flag=3;
+
+		for(var i =0; i < spiceArray.length; i++){
+			var checkBox = document.createElement("input");
+			var label = document.createElement("label");
+			var myEnter2 = document.createElement("br");
+			var myEnter1 = document.createElement("br");
+
+			checkBox.type = "checkbox";
+			checkBox.value = spiceArray[i];
+
+			myDiv.appendChild(myEnter1);
+			myDiv.appendChild(checkBox);
+			myDiv.appendChild(label);
+			myDiv.appendChild(myEnter2);
+			label.appendChild(document.createTextNode(spiceArray[i]));
+		}
+	}
+}
+
+function labelNoodle(){	
+	if(flag != 4){
+		clearDiv("input-checkbox");
+		flag=4;
+
+		for(var i =0; i < noodleArray.length; i++){
+			var checkBox = document.createElement("input");
+			var label = document.createElement("label");
+			var myEnter2 = document.createElement("br");
+			var myEnter1 = document.createElement("br");
+
+			checkBox.type = "checkbox";
+			checkBox.value = noodleArray[i];
+
+			myDiv.appendChild(myEnter1);
+			myDiv.appendChild(checkBox);
+			myDiv.appendChild(label);
+			myDiv.appendChild(myEnter2);
+			label.appendChild(document.createTextNode(noodleArray[i]));
+		}
+	}
+}
+
+function labelFruit(){
+	if(flag != 5){
+		clearDiv("input-checkbox");
+		flag=5;
+
+		for(var i =0; i < fruitArray.length; i++){
+			var checkBox = document.createElement("input");
+			var label = document.createElement("label");
+			var myEnter2 = document.createElement("br");
+			var myEnter1 = document.createElement("br");
+
+			checkBox.type = "checkbox";
+			checkBox.value = fruitArray[i];
+
+			myDiv.appendChild(myEnter1);
+			myDiv.appendChild(checkBox);
+			myDiv.appendChild(label);
+			myDiv.appendChild(myEnter2);
+			label.appendChild(document.createTextNode(fruitArray[i]));
+		}
+	}
+}
+
+function labelETC(){
+	if(flag != 6){
+		clearDiv("input-checkbox");
+		flag=6;
+
+		for(var i =0; i < etcArray.length; i++){
+			var checkBox = document.createElement("input");
+			var label = document.createElement("label");
+			var myEnter2 = document.createElement("br");
+			var myEnter1 = document.createElement("br");
+
+			checkBox.type = "checkbox";
+			checkBox.value = etcArray[i];
+
+			myDiv.appendChild(myEnter1);
+			myDiv.appendChild(checkBox);
+			myDiv.appendChild(label);
+			myDiv.appendChild(myEnter2);
+			label.appendChild(document.createTextNode(etcArray[i]));
+		}
+	}
+}
+
+
+
+
+
---
+++ regis.js
@@ -0,0 +1,93 @@
+var register = document.getElementById('register');
+var error = document.getElementById('error-message');
+var flag = 0;
+
+register.onclick = function()
+{
+	checkForm();
+}
+
+function checkForm()
+{
+	flag = 0;
+	var name = document.getElementById('name');
+	var email = document.getElementById('email');
+	var password = document.getElementById('password');
+	var address = document.getElementById('address');
+	var male = document.getElementById('male');
+	var female = document.getElementById('female');
+	var city = document.getElementById('city');
+
+	if(name.value == "")
+	{
+		error.innerHTML = "Name must be filled";
+		flag++;
+	}
+	else if(name.value.length < 3 || name.value.length > 20)
+	{
+		error.innerHTML = "Name must be between 3-20 characters";
+		flag++;
+	}
+	else if(email.value == "")
+	{
+		error.innerHTML = "Email must be filled";
+		flag++;
+	}
+	else if(email.value.indexOf('@') == -1 && email.value.indexOf('.') == -1)
+	{
+		error.innerHTML = "Email must containt @ and .";
+		flag++;
+	}
+	else if(email.value.indexOf('@')+1 == email.value.indexOf('.'))
+	{
+		error.innerHTML = "@ and . can not be neighbour";
+		flag++;
+	}
+	else if(!email.value.endsWith(".com"))
+	{
+		error.innerHTML = "Email must be ended with .com";
+		flag++;
+	}
+	else if(password.value == "")
+	{
+		error.innerHTML = "Password must be filled";
+		flag++;
+	}
+	// else if(isNaN(password))
+	// {
+	// 	error.innerHTML = "Password harus berupa angka";
+	// }
+	else if(password.value.length > 6 || password.value.length < 6)
+	{
+		error.innerHTML = "Password must be 6 digit";
+		flag++;
+	}
+	else if(address.value == "")
+	{
+		error.innerHTML = "Address must be filled";
+		flag++;
+	}
+	else if(!address.value.startsWith("Jln."))
+	{
+		error.innerHTML = "Alamat must be started with Jln.";
+		flag++;
+	}
+	else if(!male.checked && !female.checked)
+	{
+		error.innerHTML = "Gender must be selected";
+		flag++;
+	}
+	else if(city.selectedIndex == 0)
+	{
+		error.innerHTML = "City has not been selected";
+		flag++;
+	}
+	else
+	{
+		window.location = "main.html"
+	}
+
+	if(flag == 0 ){
+		error.innerHTML = "";
+	}
+}
